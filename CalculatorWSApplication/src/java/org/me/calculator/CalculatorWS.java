4b3565acebd5e0f4b7ab34549a6c58c2c4da9d79
Lucas Silvestre <lukas.silvestre@gmail.com>
2013-12-03 21:13:54 +0000

    Add a WS using Netbeans

---
+++ CalculatorWSApplication/src/java/org/me/calculator/CalculatorWS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.calculator;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author lsilvs
+ */
+@WebService(serviceName = "CalculatorWS")
+@Stateless()
+public class CalculatorWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
